68696d9041bac265a1e38a118331624511373ef6
jfinkle <doubledaffy888@gmail.com>
2016-09-30 16:39:15 -0500

    Add setup and root directory

--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
-__author__ = 'Justin Finkle'
-__email__ = 'jfinkle@u.northwestern.edu'
+from distutils.core import setup
+
+setup(
+    name='pydiffexp',
+    version='0.0.1',
+    url='https://github.com/justinfinkle/pydiffexp',
+    license='GPL3',
+    author='Justin Finkle',
+    author_email='jfinkle@u.northwestern.edu',
+    description='Differential Expression Analysis in Python'
+)
