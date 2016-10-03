f8f57e1c9a166df8a35051de1b52aa5e03cf7b71
jfinkle <doubledaffy888@gmail.com>
2016-10-03 14:09:58 -0500

    IC on develop branch

--- scripts/de_test.py
+++ scripts/de_test.py
@@ -4,7 +4,7 @@ import de_analysis as dea
 from de_analysis import DEAnalysis
 
 # Variables
-test_path = "../data/raw_data/all_data_formatted.csv"
+test_path = "/Users/jfinkle/Documents/Northwestern/MoDyLS/Python/sprouty/data/raw_data/all_data_formatted.csv"
 raw_data = pd.read_csv(test_path).set_index('TargetID')
 
 # Make the data into a hierarchical dataframe to initialize the differential expression object
@@ -21,3 +21,4 @@ c3 = "KO_0-WT_0"
 results = de_object.fit(c, p_value=0.05)
 col_order = ['Diff0', 'Diff15', 'Diff60', 'Diff120'] + results.columns.tolist()[4:]
 results = results[col_order]
+print(results)
