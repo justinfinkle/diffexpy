bbf59558720c505d56088d6f3481e464f00c64ca
jfinkle <doubledaffy888@gmail.com>
2016-10-18 16:41:27 -0500

    Make multiindex with integers for proper selection

--- scripts/de_test.py
+++ scripts/de_test.py
@@ -7,13 +7,13 @@ test_path = "/Users/jfinkle/Documents/Northwestern/MoDyLS/Python/sprouty/data/ra
 raw_data = pd.read_csv(test_path, index_col=0)
 hierarchy = ['condition', 'well', 'time', 'replicate']
 
+raw_data[raw_data <= 0] = 1
 dea = DEAnalysis(raw_data, index_names=hierarchy, reference_labels=['condition', 'time'])
 
-
+print(dea.data)
 # Types of contrasts
 c_dict = {'Diff0': "(KO_15-KO_0)-(WT_15-WT_0)", 'Diff15': "(KO_60-KO_15)-(WT_60-WT_15)",
           'Diff60': "(KO_120-KO_60)-(WT_120-WT_60)", 'Diff120': "(KO_240-KO_120)-(WT_240-WT_120)"}
 c_list = ["KO_15-KO_0", "KO_60-KO_15", "KO_120-KO_60", "KO_240-KO_120"]
 c_string = "KO_0-WT_0"
 dea.fit(c_dict)
-print(dea.get_results())
