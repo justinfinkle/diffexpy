781a2de36743e097388c9ebcb732c19816e30ac5
Weipeng Zhang <weipeng@dhcp-10-105-110-134.wireless.northwestern.private>
2017-07-26 15:20:33 -0500

    initial commit

--- scripts/main.py
+++ scripts/main.py
@@ -3,15 +3,19 @@ This is a basic example that demonstrates how to load data, do a fit
 """
 
 import pandas as pd
+import sys
+
 from pydiffexp import DEAnalysis
 
 # Load the data
-test_path = "/Users/jfinkle/Documents/Northwestern/MoDyLS/Code/Python/sprouty/data/raw_data/all_data_formatted.csv"
+test_path = "/Users/weipeng/Desktop/sprouty_microarray_data.csv"
 raw_data = pd.read_csv(test_path, index_col=0)
 hierarchy = ['condition', 'well', 'time', 'replicate']
 
 # The example data has been background corrected, so set everything below 0 to a trivial positive value of 1
 raw_data[raw_data <= 0] = 1
+print(raw_data.values)
+sys.exit
 
 # Make the Differential Expression Analysis Object
 # The reference labels specify how samples will be organized into unique values
@@ -25,4 +29,6 @@ dea.fit_contrasts()
 print(dea.results['KO_ar'].discrete_clusters.loc['FAM110C'])
 print(dea.results['WT_ar'].discrete_clusters.loc['FAM110C'])
 # dea.to_pickle("./sprouty_pickle.pkl")
+
+
 
